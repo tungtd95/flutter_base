afac26b44fca9617411969e888eb4487c37b391c
Tung VD <tungtd95@technixo.com>
2022-11-03 09:37:01 +0700

    config common dependencies

--- clean.sh
+++ clean.sh
@@ -4,3 +4,24 @@ rm -rf build
 flutter clean
 rm pubspec.lock
 flutter pub get
+
+cd flutter_base_config
+rm -rf build
+flutter clean
+rm pubspec.lock
+flutter pub get
+cd ..
+
+cd flutter_base_core_module_1
+rm -rf build
+flutter clean
+rm pubspec.lock
+flutter pub get
+cd ..
+
+cd flutter_base_module_1
+rm -rf build
+flutter clean
+rm pubspec.lock
+flutter pub get
+cd ..
