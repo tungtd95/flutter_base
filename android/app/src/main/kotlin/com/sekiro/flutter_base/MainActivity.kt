1e4661d8004d1cae5f510f0d0e642ef34bcaec44
ductung <ductung@bfast.dev>
2023-10-16 16:24:40 +0700

    implement play install referrer

--- android/app/src/main/kotlin/com/sekiro/flutter_base/MainActivity.kt
+++ android/app/src/main/kotlin/com/sekiro/flutter_base/MainActivity.kt
@@ -1,6 +1,141 @@
 package com.sekiro.flutter_base
 
+import android.os.Bundle
+import androidx.annotation.NonNull
+import com.android.installreferrer.api.InstallReferrerClient
+import com.android.installreferrer.api.InstallReferrerStateListener
+import com.android.installreferrer.api.ReferrerDetails
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.Result
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
+
+    private lateinit var channel: MethodChannel
+    private val pendingResults = arrayListOf<Result>()
+    private var referrerClient: InstallReferrerClient? = null
+    private var referrerDetails: ReferrerDetails? = null
+    private var referrerError: Pair<String, String>? = null
+
+    private val isInstallReferrerPending: Boolean
+        @Synchronized
+        get() {
+            return referrerClient != null && !isInstallReferrerResolved
+        }
+
+    private val isInstallReferrerResolved: Boolean
+        @Synchronized
+        get() {
+            return referrerDetails != null || referrerError != null
+        }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        channel = MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "de.lschmierer.android_play_install_referrer")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getInstallReferrer") {
+            getInstallReferrer(result)
+        } else {
+            result.notImplemented()
+        }
+
+    }
+
+    override fun onDestroy() {
+        pendingResults.clear()
+        referrerClient?.endConnection()
+        channel.setMethodCallHandler(null)
+        super.onDestroy()
+    }
+
+    @Synchronized
+    private fun getInstallReferrer(@NonNull result: Result) {
+        if (isInstallReferrerResolved) {
+            resolveInstallReferrerResult(result)
+        } else {
+            pendingResults.add(result)
+
+            if(!isInstallReferrerPending) {
+                referrerClient = InstallReferrerClient.newBuilder(context).build()
+                referrerClient?.startConnection(object : InstallReferrerStateListener {
+                    override fun onInstallReferrerSetupFinished(responseCode: Int) {
+                        handleOnInstallReferrerSetupFinished(responseCode)
+                    }
+
+                    override fun onInstallReferrerServiceDisconnected() {}
+                })
+            }
+        }
+    }
+
+    @Synchronized
+    private fun handleOnInstallReferrerSetupFinished(responseCode: Int) {
+        when (responseCode) {
+            InstallReferrerClient.InstallReferrerResponse.OK -> {
+                referrerClient?.let {
+                    referrerDetails = it.installReferrer
+                } ?: run {
+                    referrerError = Pair("BAD_STATE", "Result is null.")
+                }
+            }
+            InstallReferrerClient.InstallReferrerResponse.SERVICE_DISCONNECTED -> {
+                referrerError = Pair("SERVICE_DISCONNECTED", "Play Store service is not connected now - potentially transient state.")
+            }
+            InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
+                referrerError = Pair("SERVICE_UNAVAILABLE", "Connection couldn't be established.")
+            }
+            InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
+                referrerError = Pair("FEATURE_NOT_SUPPORTED", "API not available on the current Play Store app.")
+            }
+            InstallReferrerClient.InstallReferrerResponse.DEVELOPER_ERROR -> {
+                referrerError = Pair("DEVELOPER_ERROR", "General errors caused by incorrect usage.")
+            }
+            InstallReferrerClient.InstallReferrerResponse.PERMISSION_ERROR -> {
+                referrerError = Pair("PERMISSION_ERROR", "App is not allowed to bind to the Service.")
+            }
+            else -> {
+                referrerError = Pair("UNKNOWN_ERROR", "InstallReferrerClient returned unknown response code.")
+            }
+        }
+
+        resolvePendingInstallReferrerResults()
+        referrerClient?.endConnection()
+    }
+
+    @Synchronized
+    private fun resolvePendingInstallReferrerResults() {
+        pendingResults.forEach {
+            resolveInstallReferrerResult(it)
+        }
+        pendingResults.clear()
+    }
+
+    @Synchronized
+    private fun resolveInstallReferrerResult(@NonNull result: Result) {
+        referrerDetails?.let {
+            result.success(
+                mapOf(
+                    "installReferrer" to it.installReferrer,
+                    "referrerClickTimestampSeconds" to it.referrerClickTimestampSeconds,
+                    "installBeginTimestampSeconds" to it.installBeginTimestampSeconds,
+                    "referrerClickTimestampServerSeconds" to it.referrerClickTimestampServerSeconds,
+                    "installBeginTimestampServerSeconds" to it.installBeginTimestampServerSeconds,
+                    "installVersion" to it.installVersion,
+                    "googlePlayInstantParam" to it.googlePlayInstantParam
+                )
+            )
+            return
+        }
+        referrerError?.let {
+            result.error(it.first, it.second, null)
+            return
+        }
+    }
 }
--- lib/ui/home/components/weather_widget.dart
+++ lib/ui/home/components/weather_widget.dart
@@ -7,62 +7,43 @@ class WeatherWidget extends StatelessWidget {
   final void Function()? onRemoved;
   final City city;
   final Weather weather;
-  final bool isWear;
 
   WeatherWidget({
     required this.city,
     required this.weather,
     this.onSelected = null,
     this.onRemoved = null,
-    this.isWear = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (!isWear) {
-      return InkWell(
-        onTap: onSelected,
-        child: Row(
-          children: [
-            SizedBox(width: 24),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(city.getFullName()),
-                  SizedBox(height: 6),
-                  Text(
-                    '${weather.main?.temp ?? 0} Fahrenheit',
-                    style: TextStyle(color: Colors.black45),
-                  ),
-                ],
-              ),
+    return InkWell(
+      onTap: onSelected,
+      child: Row(
+        children: [
+          SizedBox(width: 24),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(city.getFullName()),
+                SizedBox(height: 6),
+                Text(
+                  '${weather.main?.temp ?? 0} Fahrenheit',
+                  style: TextStyle(color: Colors.black45),
+                ),
+              ],
             ),
-            IconButton(
-              icon: Icon(Icons.close, size: 24),
-              iconSize: 44,
-              padding: EdgeInsets.all(8),
-              onPressed: onRemoved,
-            ),
-            SizedBox(width: 16),
-          ],
-        ),
-      );
-    } else {
-      return Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(city.getFullName()),
-            SizedBox(height: 6),
-            Text(
-              '${weather.main?.temp ?? 0} Fahrenheit',
-              style: TextStyle(color: Colors.black45),
-            ),
-          ],
-        ),
-      );
-    }
+          ),
+          IconButton(
+            icon: Icon(Icons.close, size: 24),
+            iconSize: 44,
+            padding: EdgeInsets.all(8),
+            onPressed: onRemoved,
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
+    );
   }
 }
